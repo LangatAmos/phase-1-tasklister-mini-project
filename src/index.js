52c425ad61eb4ac4d7ad3828921a86c39f26edd8
Amos Langat <amos.langat@student.moringaschool.com>
2022-08-22 14:27:16 +0300

    complete assigment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener(DOMContentLoaded, () => {
+    let form = doccument.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        bildTaskList(event.target.new-task-description.value);
+        form.reset()
+    })
+
+})
+
+function bildTaskList(task){
+    let li = document.createElement('li');
+    let btn = document.createElement('button');
+    btn.addEventListener('click', handleDeleteTask);
+    btn.textContent = 'Remove';
+    li.textContent = `${task} `
+    li.appendChild(btn);
+    document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDeleteTask(e) {
+    e.target.parentNode.removeChild();
+
+};
